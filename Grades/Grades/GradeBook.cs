ab2ee991c7da920d6c134fc9006a5b08a89ecf25
rosstheslav <rswhitesnow@gmail.com>
2017-05-26 23:13:44 -0500

    Add vs project
    added all source code

---
+++ Grades/Grades/GradeBook.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    public class GradeBook
+    {
+        List<float> grades;
+        public GradeBook()
+        {
+            grades = new List<float>();
+        }
+
+
+        public void AddGrade(float grade)
+        {
+            grades.Add(grade);
+        }
+
+        public GradeStatistics ComputeStatistics()
+        {
+            GradeStatistics stats = new GradeStatistics();
+
+            float sum = 0;
+            foreach (float grade in grades)
+            {
+                //if (grade > stats.HighestGrade)
+                //{
+                //    stats.HighestGrade = grade;
+                //}
+                stats.HighestGrade = Math.Max(stats.HighestGrade, grade);
+                stats.LowestGrade = Math.Min(stats.LowestGrade, grade);
+                sum += grade;
+            }
+
+            stats.AverageGrade = sum / grades.Count;
+
+            return stats;
+        }
+    }
+}
---
+++ Grades/Grades/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(91);
+            book.AddGrade(89.5f);
+            book.AddGrade(75);
+
+            GradeStatistics stats= book.ComputeStatistics();
+
+            //GradeBook book2 = book;
+            //book2.AddGrade(75);
+
+            Console.WriteLine(stats.AverageGrade);
+            Console.WriteLine(stats.HighestGrade);
+            Console.WriteLine(stats.LowestGrade);
+
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+            synth.Speak(stats.LowestGrade+"is the lowest grade");
+        }
+    }
+}
